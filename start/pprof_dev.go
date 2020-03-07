9c95e79dde1b9589183d04fca58efda7dcb9c9cb
fox.cpp <fox.cpp@disroot.org>
2020-03-07 20:31:30 +0300

    Use runtime/trace for latency analysis

---
+++ start/pprof_dev.go
@@ -0,0 +1,17 @@
+//+build development
+
+package start
+
+import (
+	"net/http/pprof"
+
+	"github.com/gorilla/mux"
+)
+
+func addPprof(r *mux.Router) {
+	r.HandleFunc("/debug/pprof/", pprof.Index)
+	r.HandleFunc("/debug/pprof/cmdline", pprof.Cmdline)
+	r.HandleFunc("/debug/pprof/profile", pprof.Profile)
+	r.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
+	r.HandleFunc("/debug/pprof/trace", pprof.Trace)
+}
