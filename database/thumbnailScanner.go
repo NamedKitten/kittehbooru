d16e3d08867bbd18c1cf16f5440801efc0be0722
Kitteh <me@kitteh.pw>
2020-03-10 10:22:24 +0000

    Added trace stuff to thumbnailScanner

--- database/thumbnailScanner.go
+++ database/thumbnailScanner.go
@@ -3,6 +3,7 @@ package database
 import (
 	"context"
 	"fmt"
+	"runtime/trace"
 	"time"
 
 	"github.com/rs/zerolog/log"
@@ -11,22 +12,11 @@ import (
 // Thumbnail Scanner scans for posts missing thumbnails every 15 minutes
 func (db *DB) thumbnailScanner() {
 	for {
-
-		ctx := context.TODO()
+		ctx, task := trace.NewTask(context.Background(), "thumbnailScanner")
 		log.Info().Msg("thumbnailScanner scanning for posts with missing thumbnials.")
 
-		rows, err := db.sqldb.Query(`select "postid" from posts where true`)
-		if err != nil {
-			log.Error().Err(err).Msg("thumbnailScanner can't select all posts")
-		}
-		defer rows.Close()
-		var postID int64
-		for rows.Next() {
-			err = rows.Scan(&postID)
-			if err != nil {
-				log.Error().Err(err).Msg("thumbnailScanner can't scan row")
-				break
-			}
+		posts := db.cacheSearch(ctx, []string{"*"})
+		for _, postID := range posts {
 			if !db.ThumbnailsStorage.Exists(ctx, fmt.Sprintf("%d.webp", postID)) {
 				log.Debug().Int64("postID", postID).Msg("Missing thumbnail, generating new.")
 				p, exists := db.Post(ctx, postID)
@@ -34,10 +24,10 @@ func (db *DB) thumbnailScanner() {
 					log.Debug().Int64("postID", postID).Msg("thumbnailScanner can't fetch post.")
 					continue
 				}
-
 				db.CreateThumbnail(ctx, p)
 			}
 		}
+		task.End()
 
 		time.Sleep(time.Minute * 15)
 	}
